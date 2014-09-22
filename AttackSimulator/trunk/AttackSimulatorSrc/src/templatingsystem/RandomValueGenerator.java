577ad8b01c824c3c1e008415bc47a8a03be35b85
avarma <avarma@ff5db55b-8fb5-4cc2-90ed-4a08d4ef33a5>
2014-09-22 23:19:04 +0000

    Initial import of template engine

---
+++ AttackSimulator/trunk/AttackSimulatorSrc/src/templatingsystem/RandomValueGenerator.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package templatingsystem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author securonix
+ */
+public class RandomValueGenerator extends ValueGeneratorType{
+
+    public RandomValueGenerator(String variableName, ArrayList<String> params) {
+        super(variableName, params);
+    }
+
+    @Override
+    public HashMap<String, String> getValue() {
+        
+        return null;
+    }
+}
