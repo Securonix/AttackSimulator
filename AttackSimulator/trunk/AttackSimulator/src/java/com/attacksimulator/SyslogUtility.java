f84855ee2ba8bfa1a81c56ad0940b31ba3f27dfb
avarma <avarma@ff5db55b-8fb5-4cc2-90ed-4a08d4ef33a5>
2014-09-27 19:41:37 +0000

    SyslogUtility added.. This is probably what is needed to send the syslog messages. Currently only sends info. For the other types of messages, will add code later.

---
+++ AttackSimulator/trunk/AttackSimulator/src/java/com/attacksimulator/SyslogUtility.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.attacksimulator;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+import org.productivity.java.syslog4j.Syslog;
+import org.productivity.java.syslog4j.SyslogConfigIF;
+import org.productivity.java.syslog4j.SyslogIF;
+import org.productivity.java.syslog4j.impl.net.udp.UDPNetSyslogConfig;
+
+/**
+ *
+ * @author anujva
+ */
+
+public class SyslogUtility {
+	private static enum LEVEL{
+		alert, critical, debug, emergency, error, info, notice, warn
+	}
+	
+	private final Map<LEVEL, Pattern> linePatterns = new EnumMap<>(LEVEL.class);
+	private final Map<LEVEL, String> facility = new EnumMap<>(LEVEL.class);
+	private SyslogConfigIF config;
+	private SyslogIF syslog;
+	
+	public SyslogUtility(String instancename, String destinationip, String destinationport){
+		config = new UDPNetSyslogConfig(destinationip, Integer.parseInt(destinationport));
+		syslog = Syslog.createInstance(instancename, config);
+	}
+	
+	public void publishString(String str){
+		syslog.info(str);
+	}
+}
