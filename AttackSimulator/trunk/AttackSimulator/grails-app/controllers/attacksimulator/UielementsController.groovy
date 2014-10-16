04aab549002befa2c4679455341f5a4d3662346c
avarma <avarma@ff5db55b-8fb5-4cc2-90ed-4a08d4ef33a5>
2014-10-16 15:32:55 +0000

    Made a job that is capable of running my RunSyslogFeeds Thread.

--- AttackSimulator/trunk/AttackSimulator/grails-app/controllers/attacksimulator/UielementsController.groovy
+++ AttackSimulator/trunk/AttackSimulator/grails-app/controllers/attacksimulator/UielementsController.groovy
@@ -1,11 +1,19 @@
 package attacksimulator;
 
 import org.feedgeneratorgrails.Feedmaster;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.SimpleScheduleBuilder;
+
 
 class UielementsController {
+	
+	def quartzScheduler;
 
     def index() {
-    
+		System.out.println("The type of the scheduler is : "+ quartzScheduler.getClass());
+		Trigger trigger = TriggerBuilder.newTrigger().withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(2).repeatForever()).build();
+		
     }
     
     def getFeedtypes(){
--- AttackSimulator/trunk/AttackSimulator/grails-app/jobs/attacksimulator/TestJob.groovy
+++ AttackSimulator/trunk/AttackSimulator/grails-app/jobs/attacksimulator/TestJob.groovy
@@ -1,14 +1,31 @@
 package attacksimulator
 
-
+import com.attacksimulator.RunSysLogFeeds;
+import org.feedgeneratorgrails.Orders;
 
 class TestJob {
-    static triggers = {
-      simple repeatInterval: 5000l // execute job once in 5 seconds
-    }
+    static triggers = {}
 
-    def execute() {
+    def execute(context) {
         // execute job
-       System.out.println("Executed from the job");
+	def orderid = Integer.parseInt(context.mergedJobDataMap.get('orderid'));
+	if(orderid == null || orderid.isEmpty()){
+		System.out.println("Orderid is null.. quitting execution of job!!");
+		return;
+	}
+	def order = Orders.get(orderid);
+	def secuserid = order.userid;
+	def destinationip = order.destinationip;
+	def destinationport = order.destinationport;
+	def frequency = order.frequency;
+	def feedtype = order.feedtype;
+	def factorString = order.weekendfactor;
+	
+	RunSysLogFeeds th = new RunSysLogFeeds(secuserid, destinationip, destinationport, frequency, feedtype, orderid, factorString);
+	th.start();
+	order.threadid = th.getId();
+	order.save(flush: true);
+	
+	return;
     }
 }
