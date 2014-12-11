5b926f5f1f2ee6384d756b8a9920ee06e1501454
tgu <14232957+tgulati@users.noreply.github.com>
2014-12-11 01:20:34 +0000

    Multiple changes to support additional columns for internal account name aliases. Removed validation for IP address field format

--- AttackSimulator/web-app/js/environment.js
+++ AttackSimulator/web-app/js/environment.js
@@ -35,12 +35,12 @@ $(document).ready(function () {
     }
     
     function ValidateIPaddress(ipaddress) {
-        if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress))
-         {
-           return (true)
-         }
-       //alert("You have entered an invalid IP address!")
-       return (false)
+//        if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress))
+//         {
+//           return (true)
+//         }
+//       //alert("You have entered an invalid IP address!")
+       return (true)
     }
 
     $("#payment").submit(function (event) {
